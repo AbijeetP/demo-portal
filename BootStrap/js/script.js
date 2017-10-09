dc93a3df14f9f39eefd5626f21b2aaa3dfb25e6a
prasanna03 <prasanna.p@osmosys.asia>
2017-10-09 17:50:56 +0530

    TDST-8 : Added task grid. Fetching data from the grid

--- BootStrap/js/script.js
+++ BootStrap/js/script.js
@@ -1,3 +1,23 @@
-$(document).ready(function() {
-    $('#example').DataTable();
-} );
+$(document).ready(function () {
+    var table = $('#task-grid').DataTable({
+        "ajax": "http://10.0.0.160/osm-demo-api/tasks",
+        "columns": [
+            {"data": "id"},
+            {"data": "taskName"},
+            {"data": "dueDate"},
+            {"data": "createdOn"},
+            {"data": "statusName"}
+        ]
+    });
+    var $tableContainer = $(table.table().container());
+    $tableContainer.removeClass('form-inline');
+    var $cols = $tableContainer.find('.col-xs-12')
+    for (var i = 0; i <= $cols.length; i++) {
+        $($cols[i]).removeClass('col-xs-12').addClass('col-sm-12');
+    }
+    setTimeout(function () {
+        $tableContainer.find('.pagination').addClass('right-align');
+    });
+
+});
+
