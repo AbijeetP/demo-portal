fcabf9a1a4d79fc5c99d3ff920fdb4f4be6921ee
Savio <savio.b@osmosys.asia>
2017-10-10 10:10:35 +0530

    [TDST-4] - Added the dummy pie chart to the bootstrap page.

--- BootStrap/js/script.js
+++ BootStrap/js/script.js
@@ -1,23 +1,64 @@
 $(document).ready(function () {
-    var table = $('#task-grid').DataTable({
-        "ajax": "http://10.0.0.160/osm-demo-api/tasks",
-        "columns": [
-            {"data": "id"},
-            {"data": "taskName"},
-            {"data": "dueDate"},
-            {"data": "createdOn"},
-            {"data": "statusName"}
-        ]
-    });
-    var $tableContainer = $(table.table().container());
-    $tableContainer.removeClass('form-inline');
-    var $cols = $tableContainer.find('.col-xs-12')
-    for (var i = 0; i <= $cols.length; i++) {
-        $($cols[i]).removeClass('col-xs-12').addClass('col-sm-12');
+   var myBarChart = null;
+  var table = $('#task-grid').DataTable({
+    "ajax": "http://10.0.0.160/osm-demo-api/tasks",
+    "columns": [
+      { "data": "id" },
+      { "data": "taskName" },
+      { "data": "dueDate" },
+      { "data": "createdOn" },
+      { "data": "statusName" }
+    ]
+  });
+  var $tableContainer = $(table.table().container());
+  $tableContainer.removeClass('form-inline');
+  var $cols = $tableContainer.find('.col-xs-12')
+  for (var i = 0; i <= $cols.length; i++) {
+    $($cols[i]).removeClass('col-xs-12').addClass('col-sm-12');
+  }
+  setTimeout(function () {
+    $tableContainer.find('.pagination').addClass('right-align');
+  });
+
+  function randomScalingFactor() {
+    return Math.round(Math.random() * 100);
+  };
+  var config = {
+    type: 'pie',
+    data: {
+      datasets: [{
+        data: [
+          randomScalingFactor(),
+          randomScalingFactor(),
+          randomScalingFactor(),
+          randomScalingFactor(),
+          randomScalingFactor(),
+        ],
+        backgroundColor: [
+          '#ff0000',
+          '#ffa500',
+          '#ffff00',
+          '#228B22',
+          '#0000ff'
+        ],
+        label: 'Dataset 1'
+      }],
+      labels: [
+        "Red",
+        "Orange",
+        "Yellow",
+        "Green",
+        "Blue"
+      ]
+    },
+    options: {
+      responsive: true
     }
-    setTimeout(function () {
-        $tableContainer.find('.pagination').addClass('right-align');
-    });
+  };
+
+  var ctx = document.getElementById("chart-area").getContext("2d");
+  myBarChart = new Chart(ctx, config);
+
 
 });
 
