21e08342cb30c5332745d85718e119c6af94d9eb
Manohar <saimanohar.p@osmosys.asia>
2017-11-06 19:25:00 +0530

    Resolved discussions by Prasanna.

--- Common/js/script.js
+++ Common/js/script.js
@@ -5,14 +5,30 @@ $(document).ready(function () {
     "info": false,
     "order": [[0, 'asc']],
     "columnDefs": [
-      { "targets": [0, 1, 2], "orderable": false }
+      {
+        "render": function (data, type, row) {
+          return "<a href=" + row.link + ">" + row.moduleName + "</a>";
+        },
+        "width": "20%",
+        "orderable": false,
+        "targets": 0
+      }, {
+        "render": function (data, type, row) {
+          return row.desc;
+        },
+        "width": "60%",
+        "orderable": false,
+        "targets": 1
+      }, {
+        "render": function (data, type, row) {
+          return row.tech;
+        },
+        "width": "20%",
+        "orderable": false,
+        "targets": 2
+      }
     ],
     "dom": ' <"search"f><"top"l>rt<"bottom"ip><"clear">',
-    "columns": [
-      { "data": "link", "width": "20%" },
-      { "data": "desc", "width": "60%" },
-      { "data": "tech", "width": "20%" }
-    ]
   });
 
   $('.search input').addClass('form-control');
