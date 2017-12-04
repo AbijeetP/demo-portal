d7c17ffb97adf83220f6750acc687b3d57a87d1c
priyanka.a <priyanka.a@osmosys.asia>
2017-12-04 20:48:52 +0530

    Implemented validations and reset

--- Vue/src/components/AddTask.vue
+++ Vue/src/components/AddTask.vue
@@ -5,14 +5,16 @@
       <el-row>
          <el-col :span='12'>
             <el-form-item label='Task Name'>
-               <el-input  v-model="form.name" ></el-input>
+               <el-input v-validate="'required'" :class= "{'is-danger': errors.has('taskName')}" name="taskName" v-model="form.name" ></el-input>
+               <span v-show="errors.has('taskName')" class="help is-danger">{{errors.first('taskName')}}</span>
             </el-form-item>
          </el-col>
          <el-col :span='12'>
             <el-form-item label='Status'>
-               <el-select v-model='form.status' placeholder='please select status'>
+               <el-select v-validate="'required'" :class = "{'is-danger': errors.has('status')}" name = "status" v-model='form.status' placeholder='please select status'>
                   <el-option v-for="status in statusList" :label='status.statusName' :value = 'status.statusID'></el-option>
                </el-select>
+               <span v-show="errors.has('status')" class="is-danger">{{errors.first('status')}}</span>
             </el-form-item>
          </el-col>
       </el-row>
@@ -20,15 +22,16 @@
          <el-col :span='12'>
             <el-form-item label='Due date'>
                <el-col :span='24'>
-                  <el-date-picker type='date' placeholder='Pick a date' v-model='form.dueDate' style='width: 100%;'></el-date-picker>
+                  <el-date-picker  v-validate="'required|date_format'" :class = "{'is-danger': errors.has('dueDate')}" name = "dueDate" type='date' placeholder='Pick a date' v-model='form.dueDate' style='width: 100%;'></el-date-picker>
+                  <span v-show="errors.has('dueDate')" class="is-danger">{{errors.first('dueDate')}}</span>
                </el-col>
             </el-form-item>
          </el-col>
       </el-row>
       <el-row type="flex" justify="end">
          <el-form-item>
-            <el-button type='primary' @click="createNewTask()">Create</el-button>
-            <el-button>Cancel</el-button>
+            <el-button type='submit' @click="createNewTask()">Create</el-button>
+            <el-button @click="resetForm()">Cancel</el-button>
          </el-form-item>
       </el-row>
    </el-form>
@@ -39,7 +42,14 @@ import Vue from 'vue';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import { mapGetters } from 'vuex';
+import VeeValidate from 'vee-validate';
+
 Vue.use(ElementUI);
+const veeValidateConfig = {
+  fieldsBagName : 'formControls',
+  events: 'input|blur'
+}
+Vue.use(VeeValidate, veeValidateConfig);
 export default {
   name: 'AddTask',
   data() {
@@ -62,32 +72,51 @@ export default {
   },
   methods: {
     addFormValues: function() {
-      this.form.name = this.getTaskDetails.taskName;
-      this.form.status = this.getTaskDetails.statusID;
-      this.form.dueDate = this.getTaskDetails.dueDate;
-      this.form.isUpdate = this.getTaskDetails.taskName ? true : false;
+        this.form.name = this.getTaskDetails.taskName;
+        this.form.status = this.getTaskDetails.statusID;
+        this.form.dueDate = this.getTaskDetails.dueDate;
+        this.form.isUpdate = this.getTaskDetails.taskName ? true : false;
     },
     createNewTask: function() {
-      var newTaskData = {};
-      newTaskData.taskName = this.form.name;
-      newTaskData.createdOn = this.formatDate(new Date());
-      newTaskData.dueDate = this.formatDate(this.form.dueDate);
-      newTaskData.statusID = this.form.status;
-      newTaskData.statusName = this.getStatusName(this.form.status);
-      newTaskData.isUpdate = this.form.isUpdate;
-      this.$emit('addTask', newTaskData);
-      this.form.isUpdate = false;
+        this.$validator.validateAll().then((result) => {
+            if (!result) {
+                return;
+            } else {
+                var newTaskData = {};
+                newTaskData.taskName = this.form.name;
+                newTaskData.createdOn = this.formatDate(new Date());
+                newTaskData.dueDate = this.formatDate(this.form.dueDate);
+                newTaskData.statusID = this.form.status;
+                newTaskData.statusName = this.getStatusName(this.form.status);
+                newTaskData.isUpdate = this.form.isUpdate;
+                this.$emit('addTask', newTaskData);
+                this.form.isUpdate = false;
+                this.resetForm();
+            }
+        });
+    },
+    resetForm() {
+        var vm = this;
+        vm.form = {
+            name: '',
+            status: '',
+            dueDate: '',
+            isUpdate: ''
+        }
+        setTimeout(function() {
+            vm.$validator.clean();
+        }, 50)
     },
     formatDate: function(date) {
-      var dateObj = moment(date, 'DD-MM-YYYY').toDate();
-      return moment(dateObj).format('DD-MM-YYYY');
+        var dateObj = moment(date, 'DD-MM-YYYY').toDate();
+        return moment(dateObj).format('DD-MM-YYYY');
     },
     getStatusName: function(statusId) {
-      for (var i = 0; i < this.statusList.length; i++) {
-        if (this.statusList[i].statusID === +statusId) {
-          return this.statusList[i].statusName;
+        for (var i = 0; i < this.statusList.length; i++) {
+            if (this.statusList[i].statusID === +statusId) {
+                return this.statusList[i].statusName;
+            }
         }
-      }
     }
   },
   mounted: function() {
@@ -104,3 +133,12 @@ export default {
   }
 };
 </script>
+<style>
+.is-danger input,
+.is-danger select {
+  border-color: #ff3860;
+}
+.is-danger {
+ color: #ff3860;
+}
+</style>
