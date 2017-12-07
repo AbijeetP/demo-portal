6f53686693ced44a575c1e458df681f82ab688d5
priyanka.a <priyanka.a@osmosys.asia>
2017-12-07 16:41:27 +0530

    Added graphs

---
+++ Vue/src/components/TasksCompleted.vue
@@ -0,0 +1,135 @@
+<template>
+<div class="completed-on-graph-container">
+<canvas id="completedOnGraphContainer" width="540" height="300"></canvas>
+</div>
+</template>
+<script>
+import chartJs from "chart.js/dist/Chart.js";
+import { mapGetters } from "vuex";
+export default {
+  name: "TasksCompleted",
+  data: function() {
+    return {
+      lineChart: ""
+    };
+  },
+  computed: {
+    ...mapGetters(["getTasksList"])
+  },
+  watch: {
+    getTasksList: function(tasksList) {
+      var charts = {
+        labels: [],
+        data: []
+      };
+      var completedTasksCount = [];
+      tasksList = this.sortDates(tasksList);
+
+      for (var i = 0; i < tasksList.length; i++) {
+        var task = tasksList[i];
+        if (
+          task.completedOn &&
+          charts.labels.indexOf(task.completedOn) === -1
+        ) {
+          charts.labels.push(task.completedOn);
+        }
+        if (task.completedOn) {
+          completedTasksCount[task.completedOn] =
+            (completedTasksCount[task.completedOn] || 0) + 1;
+        }
+      }
+      for (var i = 0; i < charts.labels.length; i++) {
+        charts.data.push(completedTasksCount[charts.labels[i]]);
+      }
+      this.initChart(charts);
+    }
+  },
+  methods: {
+    sortDates: function(tasksList) {
+      var newTaskList = [...tasksList].sort(function(taskObj, taskObjStatus) {
+        taskObj = taskObj.completedOn
+          .split("-")
+          .reverse()
+          .join("");
+        taskObjStatus = taskObjStatus.completedOn
+          .split("-")
+          .reverse()
+          .join("");
+        return taskObj > taskObjStatus ? 1 : taskObj < taskObjStatus ? -1 : 0;
+      });
+      return newTaskList;
+    },
+    initChart: function(chartsData) {
+      var chartElement = $("#completedOnGraphContainer");
+      var chartConfig = {
+        type: "line",
+        data: {
+          labels: chartsData.labels,
+          datasets: [
+            {
+              label: "Tasks completed",
+              backgroundColor: "rgb(0, 202, 202)",
+              borderColor: "rgb(0, 202, 202)",
+              borderCapStyle: "butt",
+              pointBackgroundColor: "rgb(0, 202, 202)",
+              data: chartsData.data,
+              fill: false
+            }
+          ]
+        },
+        options: {
+          responsive: true,
+          tooltips: {
+            mode: "point",
+            intersect: true
+          },
+          hover: {
+            mode: "point",
+            intersect: true
+          },
+          scales: {
+            xAxes: [
+              {
+                display: true,
+                scaleLabel: {
+                  display: true,
+                  labelString: "Dates"
+                }
+              }
+            ],
+            yAxes: [
+              {
+                display: true,
+                scaleLabel: {
+                  display: true,
+                  labelString: "Number of tasks"
+                },
+                ticks: {
+                  stepSize: 1,
+                  beginAtZero: true,
+                  max: 10
+                }
+              }
+            ]
+          },
+          legend: {
+            onClick: function(e) {
+              e.stopPropagation();
+            }
+          }
+        }
+      };
+      if (this.lineChart) {
+        this.lineChart.destroy();
+      }
+      this.lineChart = new Chart(chartElement, chartConfig);
+    }
+  }
+};
+</script>
+<style>
+.completed-on-graph-container {
+  width: 554px;
+  height: 300px;
+}
+</style>
---
+++ Vue/src/components/TasksStatus.vue
@@ -0,0 +1,65 @@
+<template>
+<div id="tasksStatusGraphContainer">
+<canvas id="tasksStatusGraph" width="540" height="300"></canvas>
+</div>
+
+</template>
+<script>
+import chartJs from 'chart.js/dist/Chart.js'
+import {mapGetters} from 'vuex';
+export default {
+    name: 'TasksStatus',
+    computed: {
+        ...mapGetters(['getTasksList'])
+    },
+    watch: {
+        getTasksList: function(tasksList){
+            var chartInfo = {
+                labels : [],
+                tasksData: []
+            };
+            var statusInfo = [];
+            for(var i=0; i< tasksList.length; i++){
+                var task = tasksList[i];
+                if(chartInfo.labels.indexOf(task.statusName) === -1){
+                    chartInfo.labels.push(task.statusName);
+                }
+                statusInfo[task.statusName] = (statusInfo[task.statusName] || 0)+1;
+            }
+            for(var i=0; i<chartInfo.labels.length; i++){
+                chartInfo.tasksData.push(statusInfo[chartInfo.labels[i]]);
+            }
+            this.initPieChart(chartInfo);
+        }
+    }, 
+    methods: {
+        initPieChart(chartInfo){
+
+            var pieChartElement = $('#tasksStatusGraph')[0].getContext('2d');
+            var chartConfig = {
+                type : 'pie',
+                responsive: true,
+                data: {
+                    labels: chartInfo.labels,
+                    datasets: [{
+                        backgroundColor: [
+                            "#2ecc71",
+                            "#3498db",
+                            "#95a5a6",
+                            "#9b59b6"
+                        ],
+                        data: chartInfo.tasksData
+                    }]
+                }
+            }
+            var pieChart = new Chart(pieChartElement, chartConfig)
+        }
+    }
+}
+</script>
+<style>
+#tasksStatusGraphContainer {
+    width: 554px;
+    height: 300px;
+}
+</style>
